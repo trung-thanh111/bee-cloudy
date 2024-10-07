e276cbd19acc1146d4d83ebb873743f85f12e63f
baotrung72 <baotrung17122004@gmail.com>
2024-10-07 11:49:48 +0700

    login-gg

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+
+use Laravel\Socialite\Facades\Socialite; // Import Socialite
+use App\Models\User; // Import model User
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -31,6 +35,40 @@ class LoginController extends Controller
         } else {
             flash()->error('Thất bại. Email hoặc mật khẩu không đúng!');
             return redirect()->back()->withInput($request->except('password'));
+        }
+    }
+
+    // Phương thức để chuyển hướng người dùng tới Google
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    // Phương thức xử lý callback từ Google
+    public function handleGoogleCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+
+            $findUser = User::where('google_id', $user->id)->first();
+
+            if ($findUser) {
+                Auth::login($findUser);
+                return redirect()->intended('home');
+            } else {
+                $user = User::updateOrCreate([
+                    'email' => $user->email,
+                ], [
+                    'name' => $user->name,
+                    'google_id' => $user->id,
+                    'password' => bcrypt(Str::random(16)),
+                ]);
+
+                Auth::login($user);
+                return redirect()->intended('home');
+            }
+        } catch (\Exception $e) {
+            return redirect()->route('login')->with('error', 'Đăng nhập Google thất bại, vui lòng thử lại.');
         }
     }
 }
