671f4fa25aa257ef95e4017c6f2d3961685bb459
baotrung72 <baotrung17122004@gmail.com>
2024-10-11 00:06:32 +0700

    ChucNangDKGuiMail

---
+++ app/Mail/ConfirmEmail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Models\PendingUser;
+
+class ConfirmEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public $pendingUser;
+    public function __construct(PendingUser $pendingUser)
+    {
+        $this->pendingUser = $pendingUser;
+    }
+
+    public function build()
+    {
+        return $this->view('auth.mail')
+            ->with([
+                'token' => $this->pendingUser->token,
+                'email' => $this->pendingUser->email,
+            ]);
+    }
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Confirm Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/auth/mail.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xác Nhận Đăng Ký</title>
+    <style>
+        .button {
+            padding: 10px 20px;
+            color: white !important;
+            text-decoration: none;
+            font-weight: bold;
+            border-radius: 5px;
+        }
+        .button-confirm {
+            background-color: #28a745;
+        }
+        /* .button-cancel {
+            background-color: #dc3545;
+        } */
+         
+         p{
+            font-size: 18px;
+         }
+    </style>
+</head>
+<body>
+    <h2>Xác nhận đăng ký</h2>
+    <p>Xin chào <strong>{{ $pendingUser->name }},</strong></p>
+    <p >Vui lòng nhấn vào nút dưới đây để xác nhận đăng ký tài khoản bên <strong>BeeCloud</strong> của bạn:</p>
+
+    <a href="{{ route('confirm.registration', ['token' => $token]) }}" class="button button-confirm">Xác Nhận</a>
+    <p>Nếu bạn không đăng ký, hãy bỏ qua email này.</p>
+</body>
+</html>
